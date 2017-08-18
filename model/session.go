780ec404d33a3982fa16a20d7cff417d88c04e2c
Shirokov Roman <r.shirokov@corp.mail.ru>
2017-08-18 21:35:26 +0300

    [*] User session fix

--- model/session.go
+++ model/session.go
@@ -1,7 +1,12 @@
 package model
 
-type Session struct {
-	UserId  int `json:"user_id"`
-	AccessTocken string  `json:"access_tocken"`
+type ISessionUserGet interface {
+	GetUser (access_token string) (*User, error)
 }
+
+type Session struct {
+	UserId  int64 `json:"user_id,omitempty"`
+	AccessToken string  `json:"access_token,omitempty"`
+}
+
 
