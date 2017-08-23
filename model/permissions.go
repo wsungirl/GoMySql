c997b8b8ff0e96db9e0015994a51ee8732ca2bef
wsungirl <manzanilla17@bk.ru>
2017-08-23 05:38:37 +0300

    .

---
+++ model/permissions.go
@@ -0,0 +1,13 @@
+package model
+
+type IPermissions interface {
+	SetPermissions(perm *Permissions) error
+}
+
+type Permissions struct {
+	DbID int64 `json:"db_id,omitempty"`
+	UId int64 `json:"u_id,omitempty"`
+	Action string `json:"action,omitempty"`
+	Entity string `json:"entity,omitempty"`
+}
+
