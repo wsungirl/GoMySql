46990dadce2ac99ab7eff5b9faebfeec263aaebf
wsungirl <manzanilla17@bk.ru>
2017-08-23 05:34:41 +0300

    .

---
+++ model/history.go
@@ -0,0 +1,13 @@
+package model
+
+type IHistory interface {
+	AddAction(hist *History) error
+}
+
+
+type History struct {
+	DbID int64 `json:"db_id,omitempty"`
+	UId int64 `json:"u_id,omitempty"`
+	Action string `json:"action,omitempty"`
+	Entity string `json:"entity,omitempty"`
+}
