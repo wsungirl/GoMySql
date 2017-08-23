1da15d6d19a07a93b221afabd80b410797eaf9da
wsungirl <manzanilla17@bk.ru>
2017-08-23 05:36:56 +0300

    .

---
+++ db/history.go
@@ -0,0 +1,13 @@
+package db
+
+import (
+	"github.com/wsungirl/GoMySql/model"
+)
+
+func (db *DB) AddAction(hist *model.History) error {
+	_, err := db.Exec("INSERT INTO history VALUES(?,?,?,?);", hist.DbID, hist.UId, hist.Action, hist.Entity)
+	if(err != nil) {
+		return err
+	}
+	return nil
+}
