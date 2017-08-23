c0702e7c543bcd3b843f5722a644f98799c74634
wsungirl <manzanilla17@bk.ru>
2017-08-23 05:39:34 +0300

    .

---
+++ model/database.go
@@ -0,0 +1,38 @@
+package model
+
+type IDatabase interface {
+	GetDBInfo (idDb int64) (*Database, error)
+	GetDBList (idUser int64) ([]string, error)
+	GetTableInfo (idUser int64, idDb int64, tableName string) (*DbTable, error)
+	GetTableRows (idUser int64, idDb int64, tableName string, offset int64, limit int64, fields string) ([]string, error)
+	GetTableRow (idUser int64, idDb int64, tableName string, rowId int64) (string, error)
+	CreateDB (sDb *Database) error
+	CreateTable (sDbT *DbTable) error
+	//AddRow ()
+	//DeleteDb ()
+	//DeleteTable ()
+	//DeleteRow ()
+	//UpdateRow ()
+	//UpdateRows ()
+}
+
+type DbTable struct {
+	IdUser int64
+	IdDb int64
+	TableName string
+	ColumnTypes []string
+	ColumnNames []string
+	ColumnNotNull []bool
+	ColumnKey []string
+	ColumnDefault []string
+	ColumnExtra []string
+}
+
+type Database struct {
+	ID int64 `json:"id,omitempty"`
+	Uid int64 `json:"user_id,omitempty"`
+	DBname string `json:"db_name,omitempty"`
+}
+
+
+
