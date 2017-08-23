4a00b33e5e67ca5b64ea2c941c2502672e133748
wsungirl <manzanilla17@bk.ru>
2017-08-23 05:39:04 +0300

    .

---
+++ db/permissions.go
@@ -0,0 +1,13 @@
+package db
+
+import (
+	"github.com/wsungirl/GoMySql/model"
+)
+
+func (db *DB) SetPermissions(perm * model.Permissions) error {
+	_, err := db.Exec("INSERT INTO permissions VALUES(?,?,?,?);", perm.DbID, perm.UId, perm.Action, perm.Entity)
+	if(err != nil) {
+		return err
+	}
+	return nil
+}
