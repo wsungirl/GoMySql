12070ef0f4edd338c7b505fae6c14c2971294f80
wsungirl <manzanilla17@bk.ru>
2017-08-26 17:22:51 +0300

    CheckPermissions added

--- db/permissions.go
+++ db/permissions.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"github.com/wsungirl/GoMySql/model"
+	"errors"
 )
 
 func (db *DB) SetPermissions(perm *model.Permissions) error {
@@ -10,4 +11,14 @@ func (db *DB) SetPermissions(perm *model.Permissions) error {
 		return err
 	}
 	return nil
+}
+
+func (db *DB) CheckPermissions(perm * model.Permissions) (bool, error) {
+	row := db.QueryRow("SELECT id FROM permissions WHERE db_id=? and user_id=? and action=? and entity=?;", perm.DbID, perm.UId, perm.Action, perm.Entity)
+	var idDb int64
+	err := row.Scan(&idDb)
+	if err != nil {
+		return false, errors.New("Error getting DbID" + err.Error())
+	}
+	return true, nil
 }
